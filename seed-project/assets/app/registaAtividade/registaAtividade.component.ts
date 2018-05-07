c210c2600fb1e56aec3769db17a4d75c1afc512e
paparuco <38620750+paparuco@users.noreply.github.com>
2018-05-07 20:44:57 +0100

    olá
    o pedro mama

---
+++ seed-project/assets/app/registaAtividade/registaAtividade.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {RegistaAtividadeService} from "./registaAtividade.service";    
+import {Router} from "@angular/router";
+import { Atividade } from "./atividade.model";
+
+@Component({
+    selector: 'regista-atividade',
+    templateUrl: './registaAtividade.component.html'
+})
+export class RegistaAtividadeComponent implements OnInit {
+    myForm: FormGroup;
+
+    constructor(private registaAtividadeService: RegistaAtividadeService, private router: Router) {}
+
+    onSubmit() {
+        const atividade = new Atividade(
+            this.myForm.value.data_atividade,
+            this.myForm.value.tipo_atividade,
+            this.myForm.value.local_atividade,
+            this.myForm.value.crianca_associada,
+            this.myForm.value.turno,
+            localStorage.getItem('userId'),
+        );
+
+        this.registaAtividadeService.register(atividade)
+            .subscribe(
+                data => {
+                    console.log(data),
+                    window.alert("Atividade registada com sucesso!");
+                    this.router.navigateByUrl('centralPage');
+                },
+                error => console.log(error)
+            );
+        this.myForm.reset();
+    }
+
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            data_atividade: new FormControl(null, Validators.required),
+            tipo_atividade: new FormControl(null, Validators.required),
+            local_atividade: new FormControl(null, Validators.required),
+            crianca_associada: new FormControl(null, Validators.required),
+            turno: new FormControl(null,Validators.required)
+            })
+    }
+}
